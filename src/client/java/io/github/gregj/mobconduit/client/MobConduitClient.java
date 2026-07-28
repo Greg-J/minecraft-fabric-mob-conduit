a026cf7aa2cf5f8aa1188dc31223d51a37c21c31
Greg Johnson <greg.johnson@gmail.com>
2026-07-28 16:43:03 -0400

    template

---
+++ src/client/java/io/github/gregj/mobconduit/client/MobConduitClient.java
@@ -0,0 +1,10 @@
+package io.github.gregj.mobconduit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobConduitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
